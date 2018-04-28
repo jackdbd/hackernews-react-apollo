9288b9d9eba857c4fea10ba7e4cc1be583b5c3ce
jackdbd <jackdebidda@gmail.com>
2018-04-28 12:49:01 +0200

    Use GraphQL query in LinkList component

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,24 +1,38 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 import Link from "./Link";
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: "1",
-        description: "Prisma turns your database into a GraphQL API 😎 😎",
-        url: "https://www.prismagraphql.com"
-      },
-      {
-        id: "2",
-        description: "The best GraphQL client",
-        url: "https://www.apollographql.com/docs/react/"
-      }
-    ];
+    console.log(this.props.feedQuery.loading);
+    if (this.props.feedQuery && this.props.feedQuery.loading) {
+      return <div>Loading feed...</div>;
+    }
+    if (this.props.feedQuery && this.props.feedQuery.error) {
+      return <div>Error</div>;
+    }
+    const linksToRender = this.props.feedQuery.feed.links;
     return (
       <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
     );
   }
 }
 
-export default LinkList;
+// Use the parser function gql on a GraphQL query to obtain a Javascript object.
+const FEED_QUERY = gql`
+  # This is a GraphQL comment
+  query FeedQuery {
+    feed {
+      links {
+        id
+        url
+        description
+      }
+    }
+  }
+`;
+
+// Use graphql to wrap the LinkList component with the query. Pass some options.
+// Apollo will inject a new prop called feedQuery into the graphql component.
+export default graphql(FEED_QUERY, { name: "feedQuery" })(LinkList);
