70684bbdcf4a0d23e82e4bd8f44987ddf6efcec9
jackdbd <jackdebidda@gmail.com>
2018-04-29 00:26:44 +0200

    Add pagination

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
 import { FEED_QUERY } from "./LinkList";
+import { LINKS_PER_PAGE } from "../constants";
 
 class CreateLink extends Component {
   state = {
@@ -40,18 +41,24 @@ class CreateLink extends Component {
         description,
         url
       },
-      // manually update the cache (Apollo's imperative store API)
       update: (store, { data: { post } }) => {
-        const data = store.readQuery({ query: FEED_QUERY });
+        const first = LINKS_PER_PAGE;
+        const skip = 0;
+        const orderBy = "createdAt_DESC";
+        const data = store.readQuery({
+          query: FEED_QUERY,
+          variables: { first, skip, orderBy }
+        });
         data.feed.links.splice(0, 0, post);
+        data.feed.links.pop();
         store.writeQuery({
           query: FEED_QUERY,
-          data
+          data,
+          variables: { first, skip, orderBy }
         });
       }
     });
-    // automatic redirect to the '/' route
-    this.props.history.push("/");
+    this.props.history.push(`/new/1`);
   };
 }
 
